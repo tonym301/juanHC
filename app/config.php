ef0178849f2c72c1bb0b7c41da681608bcb3dfc0
tonym301 <30272917+tonym301@users.noreply.github.com>
2018-10-29 10:11:28 -0400

    f8

--- app/config.php
+++ app/config.php
@@ -2,12 +2,8 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://ec2-52-15-226-73.us-east-2.compute.amazonaws.com'); # your base URL
+define('BASE_URL','http://ec2-52-15-226-73.us-east-2.compute.amazonaws.com/juansegura'); # your base URL
 // define('BASE_URL','http://ec2-18-222-16-185.us-east-2.compute.amazonaws.com/CS3744Project4'); # base URL for server
 
-define('DB_USER', 'root');
-define('DB_PASS', 'CS3744!P4');
-define('DB_HOST', 'localhost');
-define('DB_DATABASE', 'peleliu');
 
 ?>
