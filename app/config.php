dc6653d980d54890f817cba4f03d05f9eb98f0cb
tonym301 <30272917+tonym301@users.noreply.github.com>
2018-10-29 09:26:55 -0400

    f2

--- app/config.php
+++ app/config.php
@@ -2,7 +2,7 @@
 
 // path constants
 define('SYSTEM_PATH', dirname(__FILE__)); # location of 'app' folder - don't change
-define('BASE_URL','http://standforspecialed.com'); # your base URL
+define('BASE_URL','http://www.standforspecialed.com'); # your base URL
 // define('BASE_URL','http://ec2-18-222-16-185.us-east-2.compute.amazonaws.com/CS3744Project4'); # base URL for server
 
 define('DB_USER', 'root');
